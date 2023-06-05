ab38c488c896823371c2cdab6509d3c25a4dbab3
Gerardo Quintero <jerryquinteroIII@Andrews-Mini.attlocal.net>
2023-06-05 14:59:41 -0600

    Finished full crud with method overide and pkg-lock.json fixed

--- app.js
+++ app.js
@@ -1,41 +1,73 @@
-const express = require('express'); // Start of express app configuration 
+const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}); //adding options
+});
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("Database connected");
 });
-// logic to check if there is a error or opened  and set a reffernece to shorten boiler plate. 
-// just so I dont have to mention mongoose.connection.on everytime
-
+// logic to check if there is a error or opened  and set a reference to shorten boiler plate. 
+// just so I dont have to mention mongoose.connection.on every time
 
 const app = express();
 
-app.set('view engine', 'ejs');   //setting view engine and views directory 
+
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
-app.get('/', (req, res) => {   //basic route
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+
+app.get('/', (req, res) => {
     res.render('home')
-})
+});
 app.get('/campgrounds', async (req, res) => {
-    const campgrounds = await Campground.find();
-    res.render('campgrounds/index', { campgrounds }); //first part grabs, second part renders
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds })
+});
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
 })
 
-// app.get('/makecampground', async (req, res) => {   //hardcoding fake camp ground to test 
-//    const camp = new Campground({title: 'My backyard'});
-//    await camp.save();
-//    res.send(camp)
-// })
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.get('/campgrounds/:id', async (req, res,) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/show', { campground });
+});
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/edit', { campground });
+})
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    res.redirect(`/campgrounds/${campground._id}`)
+});
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+})
+
+
 
 app.listen(3000, () => {
-    console.log('Serving opn port 30000') //basic setup, it works
+    console.log('Serving on port 3000') //check back to see if it works
 })

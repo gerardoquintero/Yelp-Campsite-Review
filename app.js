9b799c5a3c5e431f1d6de904ae2675cde4ab4bc7
Gerardo Quintero <jerryquinteroIII@Andrews-Mini.attlocal.net>
2023-06-08 11:24:27 -0600

    missing syntac and code organization

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-const { campgroundSchema} = require('./schemas.js');
+const { campgroundSchema } = require('./schemas.js');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
@@ -21,57 +21,39 @@ db.once("open", () => {
 
 const app = express();
 
-// one of many engines you can use for ejs
-
-app.engine('ejs', ejsMate)  
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
-// creating a validate middleware function schema 
 const validateCampground = (req, res, next) => {
-    
-    //  validate with req.body 
-    const{ error } = campgroundSchema.validate(body);
-    if(error){
-        const msg = error.details.map(element => element.message).join(',')
-        // if error map over the error deatils and send it string message
-        throw new ExpressError(msg, 400);
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
     } else {
         next();
-        // must absoulety call next if you ever want to make it to the next correct call in sequence
+         // must absoulety call next if you ever want to make it to the next correct call in sequence
     }
 }
 
 app.get('/', (req, res) => {
     res.render('home')
 });
-app.get('/campgrounds', async (req, res) => {
+app.get('/campgrounds', catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds })
-});
+}));
+
 app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 })
-
 //  here I created validation for the server side, so people cant be sneaky with postman lol
-app.post('/campgrounds', validateCampground, catchAsync(async (req, res) => {
-    const campgroundSchema = Joi.object({
-        campground: Joi.object({
-            title: Joi.string().required,
-            price: Joi.number().required.min(0),
-            image: Joi.string().required(),
-            location: Joi.string().required(),
-            description: Joi.string().required()
-        }).required()
-    })
-    const{ error } = campgroundSchema.validate(body);
-    if(error){
-        const msg = error.details.map(element => element.message).join(',')
-        throw new ExpressError(msg, 400);
-    }
+
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
+    // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
@@ -87,7 +69,7 @@ app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     res.render('campgrounds/edit', { campground });
 }))
 
-app.put('/campgrounds/:id',validateCampground, catchAsync(async (req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`)
@@ -97,16 +79,18 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
-}))
+}));
 
 app.all('*', (req, res, next) => {
-    next(new ExpressError('Page not found', 404))
+    next(new ExpressError('Page Not Found', 404))
 })
-app.use((err, req, res, next) => {
-    const { statusCode = 500, message = 'Something went wrong' } = err;
-    res.status(statusCode).semd(message);
 
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
 })
+
 app.listen(3000, () => {
     console.log('Serving on port 3000')
 })

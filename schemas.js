69fd4387dc76d2b41a064a57875bd2c87a3a4d81
Gerardo Quintero <jerryquinteroIII@Andrews-Mini.attlocal.net>
2023-06-12 08:23:12 -0600

    schema added

--- schemas.js
+++ schemas.js
@@ -9,4 +9,10 @@ module.exports.campgroundSchema = Joi.object({
         description: Joi.string().required()
     }).required()
 });
+
+module.exports.reviewSchema = Joi.object({
+    rating: Joi.number().required().min(1).max(5),
+    body: Joi.string().required()
+
+}).required()
 // here for our JOI uses in our main file

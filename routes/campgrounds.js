f571dc4bcd15fea23fad2b4418e261f037276b42
Gerardo Quintero <jerryquinteroIII@Andrews-Mini.attlocal.net>
2023-06-21 11:59:36 -0600

    more comments

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -38,6 +38,7 @@ router.get('/:id', catchAsync(async (req, res,) => {
     const campground = await Campground.findById(req.params.id).populate('reviews');
     if (!campground) {
         req.flash('error', 'Cannot find that campground!');
+        // instead of rendering just the template and redirect
         return res.redirect('/campgrounds');
     }
     res.render('campgrounds/show', { campground });

c9e50360795304d82681ab550c271472f17c6046
Gerardo Quintero <jerryquinteroIII@Andrews-Mini.attlocal.net>
2023-06-21 11:25:12 -0600

    public directory finished joins and mongo foxed

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')))
+// this create a public static flash from the direcotry 
 
 const sessionConfig = {
     secret: 'thisshouldbeabettersecret!',
